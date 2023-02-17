2a0babde095c931155860990df2ef1209727f519
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2023-02-17 10:44:50 +0100

    Add getPlayerChoice function

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ const choices = [
     'Paper',
     'Scissors'
 ];
+
+function getPlayerChoice() {
+    let playerChoice = prompt('Choose between Rock (1), Paper (2) and Scissors (3');
+
+    return choices[(parseInt(playerChoice) - 1) % choices.length];
+}
